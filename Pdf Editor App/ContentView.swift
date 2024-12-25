023f9a863cb685a0f2c5acd3ab0fd63974e10854
NSDeveloper18 <sbotirov600@gmail.com>
2024-12-25 19:25:16 +0500

    Initial Commit

---
+++ Pdf Editor App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pdf Editor App
+//
+//  Created by Shakhzod Botirov on 25/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pdf Editor App/Pdf_Editor_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pdf_Editor_AppApp.swift
+//  Pdf Editor App
+//
+//  Created by Shakhzod Botirov on 25/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Pdf_Editor_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
